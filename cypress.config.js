0e6c0fc7895d31fb58f092522077edf115713898
Divesh <owner@Owners-MacBook-Air.local>
2023-02-21 18:07:06 -0500

    initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+        charts: true,
+        reportPageTitle: 'divesh',
+        embeddedScreenshots: true,
+        inlineAssets: true,
+        saveAllAttempts: false,
+    },
     e2e: {
         "baseUrl": "https://google.com/",
-        "video": false,
-        "reporterOptions": {
-            "mochaFile": "cypress/results/tests-[hash].xml",
-            "toConsole": true
-        },
         setupNodeEvents(on, config) {
             // implement node event listeners here
+            require('cypress-mochawesome-reporter/plugin')(on);
         },
     },
 });
